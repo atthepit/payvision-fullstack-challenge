01b762f61f3654fd5ae1c18c7284eebac638af47
Pedro Paredes <email@pparedes.com>
2018-10-25 00:07:54 +0200

    Create transactions fixtures for testing

--- src/transactions/__tests__/transactions.tests.ts
+++ src/transactions/__tests__/transactions.tests.ts
@@ -1,40 +1,8 @@
 import axios from "axios";
-import { search, Transaction } from "../transactions";
+import { search } from "../transactions";
+import { transactions } from "../../../tests/fixtures";
 
 jest.mock("axios");
-
-const transactions: Transaction[] = [
-  {
-    action: "payment",
-    amount: 100,
-    brandId: 1060,
-    card: {
-      expiryMonth: "03",
-      expiryYear: "2020",
-      firstSixDigits: "378282",
-      lastFourDigits: "0005",
-      holderName: "Meredith Gerlach"
-    },
-    currencyCode: "USD",
-    id: "7b1c2fef-a001-460b-9d9a-7a6d849b1d5d",
-    trackingCode: "24c22a09-5a94-4552-83de-6128f360b906"
-  },
-  {
-    action: "payment",
-    amount: 100,
-    brandId: 1020,
-    card: {
-      expiryMonth: "08",
-      expiryYear: "2019",
-      firstSixDigits: "510510",
-      lastFourDigits: "5100",
-      holderName: "Ellis Deckow"
-    },
-    currencyCode: "USD",
-    id: "3d7fe7d9-0c88-42a5-997a-2bd44c8ce22d",
-    trackingCode: "caf23319-85f3-4691-8d87-0e7790733990"
-  }
-];
 
 test("should return transactions list", async () => {
   const resp = { data: transactions };
